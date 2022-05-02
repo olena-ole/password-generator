ce54a6e3f9409f3f07d46c8223506d9c51a394df
Lennna <pampam32121@gmail.com>
2022-05-03 00:48:41 +0300

    added short and long password options

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict'
 
-const generateBtn = document.querySelector('#generate-btn');
+const generateShortBtn = document.querySelector('#generate-short');
+const generateLongBtn = document.querySelector('#generate-long');
 const inputsArr = document.querySelectorAll('input');
 
 function generateRandomSymbol(min = 33, max = 126) {
     return String.fromCharCode(Math.floor(Math.random() * (max - min) + min));
-}
+};
 
 function generatePass(num) {
     let password = '';
@@ -13,11 +14,17 @@ function generatePass(num) {
         password += generateRandomSymbol();
     }
     return password;
-}
+};
 
 
-generateBtn.addEventListener('click', () => {
+generateShortBtn.addEventListener('click', () => {
+    inputsArr.forEach(el => {
+        el.value = generatePass(8);
+    })
+});
+
+generateLongBtn.addEventListener('click', () => {
     inputsArr.forEach(el => {
         el.value = generatePass(16);
     })
-})
+});
